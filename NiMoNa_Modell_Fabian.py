5effad00167162558366fedc240074666375676f
FaBremer <74683682+FaBremer@users.noreply.github.com>
2021-01-09 20:53:17 +0100

    Added matrix transformation functions
    Main addition: create network of influencers (see function influencer_network)

--- NiMoNa_Modell_Fabian.py
+++ NiMoNa_Modell_Fabian.py
@@ -37,28 +37,68 @@ def wheel(N):
     A[0, N-2] = 1
     return A
 
-def everybody_connected(N):
+def mesh(N):
     A = np.ones((N, N))
     for i in range(N):
         A[i, i] = 0
     return A
 
 #*******************************************************
-#Initial Data
-# Number of nodes
-N = 13
-# Initialize N nodes randomly, with opinions centered around 0
-xs = (np.random.uniform(size=N)-0.5)*2
+#Matrix transformation functions
+
+def enlarge_matrix(A):
+    #takes matrix of any shape as input and returns adj. matrix with one row and one columns of zeros added
+    B = np.ones([0,A.shape[1]+1])
+    for row in A:
+        B = np.append(B, [np.append(row, 0)], 0)
+    B = np.append(B, [np.zeros(A.shape[1]+1)], 0)
+    return B
+
+def enlarge_matrix_N(A, n):
+    #enlarge given matrix by n rows and columns of zeros
+    for i in range(n):
+        A = enlarge_matrix(A)
+    return A
+
+def make_connection(A, id1, id2):
+    A[id1][id2] = 1
+    A[id2][id1] = 1
+
+def delete_connection(A, id1, id2):
+    A[id1][id2] = 0
+    A[id2][id1] = 0
+
+def add_followers(A, i, n):
+    #adds n followers (star topology) of agent i to adj. matrix A and returns new adj. matrix
+    old_n = A.shape[1]
+    A = enlarge_matrix_N(A, n)
+    for j in range(n):
+        make_connection(A, i, old_n+j)
+    return A
+
+def influencer_network(N, m):
+    #create network of n connected agents with m followers,
+    #where m is of type int (adding m followers to each agent) or list of len(N) of ints (adding m[i] followers to agent i)
+    A = mesh(N)
+    for i in range(N):
+        if isinstance(m, int):
+            A = add_followers(A, i, m)
+        else:
+            A = add_followers(A, i, m[i])            
+    return A
+
+#*******************************************************
 
 
-#def rk4(f):
-#    return lambda t, x, h: (lambda k1: (lambda k2: (lambda k3: (lambda k4: (k1 + 2*k2 + 2*k3 + k4)/6)( h * f( t + h  , x + k3 )))( h * f( t + h/2, x + k2/2 )))( h * f( t + h/2, x + k1/2)))( h * f( t, x) )
-def rk4a(f, t, x, h, par):
+#runge-kutta-4 function
+def rk4(f, t, x, h, par):
     k1 = f(t,x,par)
     k2 = f(t + 0.5*h, x + 0.5*h*k1,par)
     k3 = f(t + 0.5*h, x + 0.5*h*k2,par)
     k4 = f(t + h, x + h*k3,par)
     return (h*(k1 + 2*k2 + 2*k3 + k4)/6)
+
+#right hand side of ODE
 def rhs(t,x,par):
     d = par[0]
     u = par[1]
@@ -73,9 +113,20 @@ def rhs(t,x,par):
         result[i] = -d*x[i]+u*np.tanh(alpha*x[i]+gamma*sum_vec[i])+b
     return result
 
-def plot(f, N):
-    A = f(N)
-    pars = [1,0.31,1.2,-1.3,0,A]
+def plot(f, par, *N):
+    print(f"Now plotting {f.__name__}.")    
+    if f.__name__ == "influencer_network":
+        A = f(N[0], N[1])
+        if isinstance(N[1], int):
+            m = N[0]+N[0]*N[1]
+        else:
+            m = N[0]+sum(N[1])
+    else:
+        m = N[0]
+        A = f(m)
+    # Initialize N nodes randomly, with opinions centered around 0
+    xs = (np.random.uniform(size=(m))-0.5)*2
+    par.append(A)
     #define other parameters:
     t_span = [0,7]
     t, x = t_span[0], xs
@@ -93,19 +144,30 @@ def plot(f, N):
     cmap = plt.get_cmap('coolwarm')
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-
     fig = plt.figure()
     camera = Camera(fig)
     while t <= t_span[1]:
         nx.draw(gr, pos=plot_positions, node_size=500, node_color=x, cmap='coolwarm', vmin=vmin, vmax=vmax)
         camera.snap()
-        t, x = t + h, x + rk4a(rhs, t, x, h, pars)
+        t, x = t + h, x + rk4(rhs, t, x, h, par)
     plt.colorbar(sm)
     animation = camera.animate()
     animation.save(f'{f.__name__}.gif', writer='PillowWriter', fps=3)
+    par.pop()
+    print(f"Saved {f.__name__}.gif")
 
-#make sure len(x)==N
-plot(wheel, N)
-plot(star, N)
-plot(circle, N)
-plot(everybody_connected, N)
+
+#Initial Data
+# Number of nodes
+N = 13
+#parameters in order d, u, alpha, gamma, b
+params = [1,0.31,1.2,-1.3,0]
+
+
+plot(wheel, params, N)
+plot(star, params, N)
+plot(circle, params, N)
+plot(mesh, params, N)
+N = 3
+m = [4,5,6]
+plot(influencer_network, params, N, m)
